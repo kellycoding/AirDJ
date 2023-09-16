3a36b69ef72c6967b5f40f4f90c37b87da61dce8
Kelly Li <43053215+kellycoding@users.noreply.github.com>
2023-09-16 09:36:13 -0400

    content to index

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, render_template
 from werkzeug.exceptions import abort
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
